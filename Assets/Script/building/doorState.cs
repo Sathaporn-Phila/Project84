334310eea8c6ca694aa4c9a3051c771f9ecae031
Sathaporn-Phila <nuinee_2544@hotmail.com>
2022-11-06 02:30:49 +0700

    Override animation locker clip from electric door clip

--- Assets/Script/building/doorState.cs
+++ Assets/Script/building/doorState.cs
@@ -1,22 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class doorState : MonoBehaviour
 {
     // Start is called before the first frame update
     Animator m_animator;
-    Transform doorLeft;
-    void Start()
-    {
+    private void Awake() {
         m_animator = GetComponent<Animator>();
-        doorLeft = transform.Find("door.armature/left.door/door.l");
     }
-
     // Update is called once per frame
     void Update()
     {
         m_animator.SetInteger("doorState",1);
-        Debug.Log(doorLeft.position);
+    }
+    public RuntimeAnimatorController getAnimController{
+        get {
+            return GetComponent<Animator>().runtimeAnimatorController;
+        }
     }
 }

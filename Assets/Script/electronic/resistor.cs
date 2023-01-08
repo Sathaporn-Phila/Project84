412f8791aa5e82e66186582883534a925a432aea
Sathaporn-Phila <nuinee_2544@hotmail.com>
2023-01-09 00:05:43 +0700

    can change color of resistor by random choice

--- Assets/Script/electronic/resistor.cs
+++ Assets/Script/electronic/resistor.cs
@@ -1,52 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Collections;
+using Unity.Jobs;
 using System.Linq;
 public class resistor : MonoBehaviour
 {
+    MaterialPropertyBlock mpb;
+    public MaterialPropertyBlock Mpb {
+        get {
+            mpb = mpb == null? new MaterialPropertyBlock():mpb;
+            return mpb;
+        }
+    }
     public class Sticker {
-        public string colorName{get;set;}
-        public Color color{get;set;}
-        public float Value{get;set;}
+        public string colorName;
+        public Color color;
+        public float Value;
         public Sticker(string name,Color col,float val ){
             colorName = name;
             color = col;
             Value = val;
         }    
     }
-    private class Attribute {
-        public List<Sticker> allSticker{get;set;}
+    public class Attribute {
+        public List<Sticker> allSticker;
         public float val = 0;
-        public Attribute(List<Sticker> stickers){
-            allSticker = stickers;
-            for(int i=0;i<allSticker.Count-1;i++){
-                val = (i<2)? (val+allSticker[i].Value*Mathf.Pow(10,i)) : (val*Mathf.Pow(10,allSticker[i].Value));
+        private int width=1024,height=1024;
+        private Dictionary<string,Sticker> oneOfthreeBar,fourthBar;
+        public Attribute(Dictionary<string,Sticker> stickers){//ค่า r
+            allSticker = new List<Sticker>();
+            //allSticker = stickers;
+            oneOfthreeBar = stickers.Where(x=>x.Key!="gold"&&x.Key!="silver").ToDictionary(c => c.Key, c => c.Value);
+            fourthBar = stickers.Where(x=>!oneOfthreeBar.Contains(x)).ToDictionary(c => c.Key, c => c.Value);
+            Debug.Log(oneOfthreeBar.Count);
+
+            //NativeArray<JobHandle> jobs = new NativeArray<JobHandle>();
+            for(int bar=0;bar<4;bar++){
+                if(bar<3){
+                    Sticker stickerProp = oneOfthreeBar.ElementAt(Random.Range(0,oneOfthreeBar.Count-1)).Value;   
+                    val = (bar<2)? (val+stickerProp.Value*Mathf.Pow(10,bar)) : (val*Mathf.Pow(10,stickerProp.Value));
+                    allSticker.Add(stickerProp);
+                }else{
+                    Sticker stickerProp = fourthBar.ElementAt(Random.Range(0,fourthBar.Count-1)).Value;
+                    allSticker.Add(stickerProp);   
+                }
             }
+            
+        }
+        public List<Vector4> getAllColor(){
+            return allSticker.Select(obj=>new Vector4(obj.color.r,obj.color.g,obj.color.b,obj.color.a)).ToList();
         }
     }
-    private Dictionary<string,Sticker> oneOfthreeBar,fourthBar;
+    Attribute prop;
     private Dictionary<string,Sticker> stickerData = new Dictionary<string, Sticker>(){
         {"black",new Sticker("black",Color.black,0)},
-        {"brown",new Sticker("brown",new Color(210,105,30),1)},
+        {"brown",new Sticker("brown",new Color((float)210/255,(float)105/255,(float)30/255),1)},
         {"red",new Sticker("red",Color.red,2)},
-        {"orange",new Sticker("orange",new Color(255,165,0),3)},
+        {"orange",new Sticker("orange",new Color((float)255/255,(float)165/255,0),3)},
         {"yellow",new Sticker("yellow",Color.yellow,4)},
         {"green",new Sticker("green",Color.green,5)},
         {"blue",new Sticker("blue",Color.blue,6)},
         {"magenta",new Sticker("magenta",Color.magenta,7)},
         {"grey",new Sticker("grey",Color.grey,8)},
         {"white",new Sticker("white",Color.white,9)},
-        {"gold",new Sticker("gold",new Color(255,215,0),0.05f)},
-        {"silver",new Sticker("silver",new Color(255,215,0),0.1f)},
+        {"gold",new Sticker("gold",new Color((float)255/255,(float)215/255,0),0.05f)},
+        {"silver",new Sticker("silver",new Color((float)192/255,(float)192/255,(float)192/255),0.1f)},
     };
-    
     void Awake(){
-        oneOfthreeBar = stickerData;
-        oneOfthreeBar = oneOfthreeBar.Where(x=>x.Key!="gold"||x.Key!="silver").ToDictionary(c => c.Key, c => c.Value);
-        fourthBar = stickerData.Where(x=>!oneOfthreeBar.Contains(x)).ToDictionary(c => c.Key, c => c.Value);
+        //กรองค่า r และความเบี่ยงเบน
+        prop = new Attribute(stickerData);
+        foreach(Sticker attr in prop.allSticker){
+            Debug.Log(attr.colorName+new Vector4(attr.color.r,attr.color.g,attr.color.b));
+        }
+        MeshRenderer renderer = GetComponent<MeshRenderer>();
+        Mpb.SetVectorArray("_ColorArray",prop.getAllColor());
+        for(int i=0;i<renderer.materials.Length;i++){
+            //if(renderer.materials[i].HasProperty("_ColorArray")){
+                renderer.SetPropertyBlock(Mpb,i);
+            //}
+            //}
+        }
 
-        
-        
     }
     void Start()
     {
--- Assets/Script/player/playerMovement.cs
+++ Assets/Script/player/playerMovement.cs
@@ -1,26 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using System.IO;
 
 public class playerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     Vector3 nextMovement;
-    Quaternion rotation = Quaternion.identity;
-    Quaternion desiredforward;
+    Quaternion rotation,desiredforward = Quaternion.identity;
     private Rigidbody rb;
     private BoxCollider hitbox;
     [SerializeField]
-    private float speed = 2f;
+    private float speed = 2f,turnSpeed = 2f;
     [SerializeField]
-    private float turnSpeed = 2f;
     private InputManager inputManager;
+    //[Serializable]
+    public class PlayerData {
+        public Vector3 position;
+        public PlayerData(Vector3 pos){
+            position = pos;
+        } 
+    }
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         hitbox = GetComponent<BoxCollider>();
         inputManager = InputManager.Instance;
         float distGround = hitbox.bounds.min.y;
+        
+
+        /*if(PlayerPrefs.HasKey("Player Position X")){
+            Vector3 position = new Vector3(
+                                            PlayerPrefs.GetFloat("Player Position X"),
+                                            PlayerPrefs.GetFloat("Player Position Y"),
+                                            PlayerPrefs.GetFloat("Player Position Z"));
+            rb.MovePosition(position);
+        }*/
     }
     bool isGround(){
         return Physics.Raycast(transform.position,-Vector3.up,1f);
@@ -55,6 +71,23 @@ public class playerMovement : MonoBehaviour
         rotate(nextMovement);
         //Debug.Log(nextMovement);
     }
-    
-    
+    private void OnApplicationQuit() { //เก็บข้อมูล player เมื่อออก
+        savePlayerPosition();
+
+    }
+    private void savePlayerPosition(){
+        PlayerData data = new PlayerData(rb.position);
+        string filename = "player.json";
+        string jsonData = JsonUtility.ToJson(data);
+        Debug.Log(jsonData);
+        File.WriteAllText(Directory.GetCurrentDirectory()+"/Assets/Script/player/"+filename,jsonData);
+        //string jsonData = JsonUtility.ToJson(data);
+        
+        
+        
+        /*PlayerPrefs.SetFloat("Player Position X",rb.position.x);
+        PlayerPrefs.SetFloat("Player Position Y",rb.position.y);
+        PlayerPrefs.SetFloat("Player Position Z",rb.position.z);
+        PlayerPrefs.Save();*/
+    }
 }

66917d22bdbf5746f91c773a27836937b16c920f
Sathaporn-Phila <nuinee_2544@hotmail.com>
2023-01-09 00:05:43 +0700

    can change color of resistor by random choice

--- Assets/Script/building/Roof.cs
+++ Assets/Script/building/Roof.cs
@@ -10,27 +10,38 @@ public class Roof : MonoBehaviour
     Query query;
     Regex rgx;
     private List<GameObject> roofs;
+    MaterialPropertyBlock mpb;
     string pattern = @"\bcyberRoof";
     // Start is called before the first frame update
+    static readonly int shaderProperties = Shader.PropertyToID("_EmissionColor");
+    public MaterialPropertyBlock Mpb {
+        get {
+            mpb = mpb == null? new MaterialPropertyBlock():mpb;
+            return mpb;
+        }
+    }
     private void SetMaterial(GameObject obj){
+        Debug.Log(obj.name);
         Renderer meshRenderer = obj.GetComponent<Renderer>();
-        meshRenderer.material.SetColor("_EmissionColor",colorPattern);
+        
+        Mpb.SetColor(shaderProperties,colorPattern);
+        meshRenderer.SetPropertyBlock(Mpb);
     }
     void Awake(){
-    }
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         query = this.gameObject.AddComponent<Query>();
         rgx = new Regex(pattern);
         roofs = query.queryByName(this.gameObject,rgx);
         foreach(GameObject item in roofs){
             SetMaterial(item);
         }
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
     }
 }

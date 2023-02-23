f9179cf495ed597337c5475f63a57b22d043b781
PannawatKingkaew <s6201012620163@email.kmutnb.ac.th>
2023-02-23 19:43:43 +0700

    add script in start_scene

---
+++ Assets/Script/Exit.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    public GameObject confirmation_panel;
+    public void button_exit()
+    {
+    
+        Application.Quit();
+
+    }
+    public void enable_confirmation()
+    {
+        confirmation_panel.SetActive(true);
+    }
+    public void disable_confirmation()
+    {
+        confirmation_panel.SetActive(false);
+    }
+}

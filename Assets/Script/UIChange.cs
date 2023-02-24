2c010f6279ce502a6ea59e9b6d4a8f5bcc3e558b
PannawatKingkaew <s6201012620163@email.kmutnb.ac.th>
2023-02-24 15:38:55 +0700

    add change ui script

---
+++ Assets/Script/UIChange.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIChange : MonoBehaviour
+{
+    public GameObject Option_UI;
+
+    public void open_option()
+    {
+        Option_UI.SetActive(true);
+    }
+    public void close_option()
+    {
+        Option_UI.SetActive(false);
+    }
+}

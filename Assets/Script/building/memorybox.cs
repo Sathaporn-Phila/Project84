88ec66356116dcf5253fa00e9c472e7ecf0cf4fa
Sathaporn-Phila <nuinee_2544@hotmail.com>
2023-03-12 11:18:11 +0700

    delete human

--- Assets/Script/building/memorybox.cs
+++ Assets/Script/building/memorybox.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[ExecuteAlways]
 public class memorybox : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    MeshFilter meshFilter;
+    GameObject fresnelObj;
+    public enum chooseMesh {
+        resistor,diode,gate
     }
-
-    // Update is called once per frame
-    void Update()
-    {
+    public chooseMesh selectMesh = new();
+    private void Start() {
+        fresnelObj = this.transform.Find("memory").gameObject;
+        meshFilter = fresnelObj.GetComponent<MeshFilter>();
         
+        if(selectMesh == chooseMesh.resistor){
+            meshFilter.mesh = Resources.Load<GameObject>("Prefabs/electronic/resistor").GetComponent<MeshFilter>().sharedMesh;
+            fresnelObj.transform.localScale = Vector3.one*0.7f;
+        }else if(selectMesh == chooseMesh.diode){
+            meshFilter.mesh = Resources.Load<GameObject>("Prefabs/electronic/diode").GetComponent<MeshFilter>().sharedMesh;
+            fresnelObj.transform.localScale = Vector3.one;
+        }else if(selectMesh == chooseMesh.gate){
+            meshFilter.mesh = Resources.Load<GameObject>("Prefabs/electronic/gate.machine.module/gate.and").GetComponent<MeshFilter>().sharedMesh;
+            fresnelObj.transform.localScale = Vector3.one*0.5f;
+        }
     }
+    
 }

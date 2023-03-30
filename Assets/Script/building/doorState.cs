d4f00635b47997f1541f71bdf3232c7f0557a5cc
PARINYA JAIHARN <s6201012620155@email.kmutnb.ac.th>
2023-03-30 23:11:59 +0700

    animation, damage, door, soundfx
    animation(main char, creature char, auto door), damage system,

--- Assets/Script/building/doorState.cs
+++ Assets/Script/building/doorState.cs
@@ -2,19 +2,68 @@ using UnityEngine;
 
 public class doorState : MonoBehaviour
 {
-    //Start is called before the first frame update
-    Animator m_animator;
-    private void Awake() {
-        m_animator = GetComponent<Animator>();
+    public Vector3 endpos;
+    public float speed = 1.0f;
+
+    private bool moving = false;
+    private bool opening = true;
+    private Vector3 startPos;
+    private float delay = 0.0f;
+    
+    void Start()
+    {
+        startPos = transform.position;
     }
-    // Update is called once per frame
+
     void Update()
     {
-        m_animator.SetInteger("doorState",1);
+        if(moving)
+        {
+            if(opening)
+            {
+                MoveDoor(endpos);
+            }
+            else
+            {
+                MoveDoor(startPos);
+            }
+        }
     }
+    
     public RuntimeAnimatorController getAnimController{
         get {
             return GetComponent<Animator>().runtimeAnimatorController;
         }
+    }
+
+    void MoveDoor(Vector3 goalPos)
+    {
+        float dist = Vector3.Distance(transform.position, goalPos);
+        if(dist > .1f)
+        {
+            transform.position = Vector3.Lerp(transform.position, goalPos, speed * Time.deltaTime);
+        }
+        else
+        {
+            if(opening)
+            {
+                delay += Time.deltaTime;
+                if(delay > 1.5f)
+                {
+                    opening = false;
+                }
+            }
+            else
+            {
+                moving = false;
+                opening = true;
+            }
+        }
+    }
+
+    public bool Moving
+    {
+        get {return moving;}
+        set {moving = value;}
     }
 }

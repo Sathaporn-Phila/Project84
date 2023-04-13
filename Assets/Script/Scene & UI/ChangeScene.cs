a4fab3cf0e63bbee4a40f77c9fba5e5a1c8ff9d6
PannawatKingkaew <s6201012620163@email.kmutnb.ac.th>
2023-04-13 15:52:30 +0700

    add transition ro changescreen  script

--- Assets/Script/Scene & UI/ChangeScene.cs
+++ Assets/Script/Scene & UI/ChangeScene.cs
@@ -5,13 +5,52 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-    public void Start_Scene() {  
+    public FadeScreen fadeScreen;
+    
+    public void GoToScene(int sceneindex)
+    {
+        StartCoroutine(GotoSceneRoutine(sceneindex));
+    }
+
+    IEnumerator GotoSceneRoutine(int sceneindex)
+    {
+        fadeScreen.FadeOut();
+        yield return new WaitForSeconds(fadeScreen.fadeDuration);
+        SceneManager.LoadScene(sceneindex);
+    }
+
+    public void GoToSceneAsync(int sceneindex)
+    {
+        StartCoroutine(GotoSceneAsyncRoutine(sceneindex));
+    }
+
+    IEnumerator GotoSceneAsyncRoutineA(int sceneindex)
+    {
+        fadeScreen.FadeOut();
+        yield return new WaitForSeconds(fadeScreen.fadeDuration);
+        AsynceOperation operation = SceneManager.LoadSceneAsync(sceneindex);
+        operation.allowSceneActivition = false;
+
+        float timer = 0
+        while(timer <= fadeScreen.fadeDuration && !operation.isDone)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        operation.allowSceneActivition = true;
+    }
+
+    public void Start_Scene() 
+    {  
         SceneManager.LoadScene("Start_Scene");  
     }  
-    public void Tutorial_Scene() {  
+    public void Tutorial_Scene() 
+    {  
         SceneManager.LoadScene("Tutorial_Scene");  
     }  
-    public void Building() {  
+    public void Building() {
+
         SceneManager.LoadScene("84Building");  
     }  
 }

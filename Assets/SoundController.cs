0b1d60b1d0e12730c1e03255d1d9ea02df935efc
PARINYA JAIHARN <s6201012620155@email.kmutnb.ac.th>
2023-04-15 21:36:25 +0700

    add characters sounds

---
+++ Assets/SoundController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    public AudioSource source;
+
+    public AudioClip footstep, run, attack, hit, scream, dying;
+
+    public void footSound()
+    {
+        source.clip = footstep;
+        source.Play();
+    }
+
+    public void runSound()
+    {
+        source.clip = run;
+        source.Play();
+    }
+
+    public void attackSound()
+    {
+        source.clip = attack;
+        source.Play();
+    }
+
+    public void hitSound()
+    {
+        source.clip = hit;
+        source.Play();
+    }
+
+    public void screamSound()
+    {
+        source.clip = scream;
+        source.Play();
+    }
+
+    public void dyingSound()
+    {
+        source.clip = dying;
+        source.Play();
+    }
+}

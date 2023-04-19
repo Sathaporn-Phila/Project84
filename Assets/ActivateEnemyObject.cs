928f93a9b6a0c921f459c1917a95ddd7469a6d91
PARINYA JAIHARN <s6201012620155@email.kmutnb.ac.th>
2023-04-20 02:15:51 +0700

    Set enable and disable enemy

---
+++ Assets/ActivateEnemyObject.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateEnemyObject : MonoBehaviour
+{
+    public GameObject enemy;
+    public BoxCollider player;
+
+    BoxCollider door;
+
+    public bool enemyIsActive;
+    public bool toggleActive;
+
+    void Start()
+    {
+        door = GetComponent<BoxCollider>();
+        player = GameObject.Find("Robot Kyle").GetComponent<BoxCollider>();
+        enemyIsActive = false;
+        enemy.SetActive(false);
+        toggleActive = false;
+    }
+
+    void Update () 
+    {
+        if (toggleActive)
+        {
+            enemyIsActive = true;
+            enemy.SetActive(true);
+        }
+        else
+        {
+            enemyIsActive = false;
+            enemy.SetActive(false);
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (enemyIsActive == false)
+        {
+            enemyIsActive = true;
+            enemy.SetActive(true);
+        }
+        else
+        {
+            enemyIsActive = false;
+            enemy.SetActive(false);
+        }
+    }
+}
---
+++ Assets/DeactivateEnemyObject.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateEnemyObject : MonoBehaviour
+{
+    public GameObject enemy;
+    public BoxCollider player;
+    public GameObject activateDoor;
+
+    public ActivateEnemyObject activator;
+
+    CapsuleCollider teleport;
+
+    public bool toggleDeactive;
+
+    void Start()
+    {
+        teleport = GetComponent<CapsuleCollider>();
+        player = GameObject.Find("Robot Kyle").GetComponent<BoxCollider>();
+        activator = activateDoor.GetComponent<ActivateEnemyObject>();
+    }
+
+    void Update () 
+    {
+        if (toggleDeactive)
+        {
+            if (enemy.activeSelf)
+            {
+                enemy.SetActive(false);
+                activator.toggleActive = false;
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (enemy.activeSelf)
+        {
+            enemy.SetActive(false);
+        }
+    }
+}
